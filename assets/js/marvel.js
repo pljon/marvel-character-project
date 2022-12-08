36a730c5b373f57f34540a569abed75c60b40b66
Jonathan Plaras <jonplaras@gmail.com>
2022-12-07 21:37:13 -0500

    added display wiki function

--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -1,15 +1,16 @@
 // marvel api keys
 var key = '17df38d3f4b222ca109def2087cb8269';
-var privateKey = '0911a2704889d61c5d4788bcb2dcc039e08e777c';
+// var privateKey = '0911a2704889d61c5d4788bcb2dcc039e08e777c';
 
 // hash via md5 generator
 var hash = '700588d4fba17a2c0d05eb1d655c1984';
 
 // for characters
-var base = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=${key}&hash=${hash}`
+// https://gateway.marvel.com/v1/public/characters?name=timmyturner&ts=1&apikey=17df38d3f4b222ca109def2087cb8269&hash=700588d4fba17a2c0d05eb1d655c1984
 
 // only 1 input element, use as variable
 var characterInput = document.querySelector('input');
+
 
 // handles any submit, any query for a character
 var formSubmit = function(event) { 
@@ -18,14 +19,16 @@ var formSubmit = function(event) {
     
     // variable for input field value aka searched marvel character
     var characterName = characterInput.value.trim();
+    characterForm.reset();
 
     // sends value to getCharacterRepos to search for character in api
     if(characterName) {
         getCharacterRepos(characterName);
+        getWikiRepos(characterName);
     }
 }
 
-// right now that character parameter is hulk
+// right now that character parameter is 
 var getCharacterRepos = function(character) {
     var apiUrl = `https://gateway.marvel.com/v1/public/characters?name=${character}&ts=1&apikey=${key}&hash=${hash}`
 
@@ -35,13 +38,14 @@ var getCharacterRepos = function(character) {
             console.log('success!')  
                 return response.json()
                     .then(function(data) {
-                        console.log(data.data.results);
+                        console.log(data.data.count);
                         var results = data.data.results;
-                        console.log(results);
-                        displayCharacterCard(results)
+                        console.log(results);                       
+                        displayCharacterCard(results);
             });
         });         
     };
+
             
 // function to display card underneath input field
 // todo: append new card under #results, append character info into card
@@ -51,14 +55,22 @@ var displayCharacterCard = function(searchedCharacter) {
         console.log(searchedCharacter[0].thumbnail.path +'.jpg');
         console.log(searchedCharacter[0].name);
         console.log(searchedCharacter[0].description);
-        // need to create elements below
+        
+        var cardcontainer = document.getElementById('resultcard');
+        cardcontainer.setAttribute('display', 'visible');
+
+        var thumbnail = searchedCharacter[0].thumbnail.path +'.jpg';
+        document.getElementById("thumbnail").src = `${thumbnail}`;
+
+        var resultcard = document.querySelector('#resultcard');
+
+
+
+        
     };
 };
-
 
 var characterForm = document.querySelector('form');
 
 // event listener when user searches character name
-// add something to clear input field
 characterForm.addEventListener('submit', formSubmit);
-
